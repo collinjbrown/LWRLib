82c7d26396b1bb32222a05f453dc85c77ea2d9e9
Collin Brown <CJBrown7966@gmail.com>
2022-10-13 13:57:57 -0700

    Update inputhandler.cpp

--- src/inputhandler.cpp
+++ src/inputhandler.cpp
@@ -30,11 +30,11 @@ namespace LWRL
 			{
 				states->sumMoveTime = 0.0f;
 
-				if (moveUp) states->cameraPosition.y += (settings->tileWidth / states->zoom);
-				else if (moveDown) states->cameraPosition.y -= (settings->tileWidth / states->zoom);
+				if (moveUp) states->cameraPosition.y += settings->tileWidth;
+				else if (moveDown) states->cameraPosition.y -= settings->tileWidth;
 
-				if (moveRight) states->cameraPosition.x += (settings->tileWidth / states->zoom);
-				else if (moveLeft) states->cameraPosition.x -= (settings->tileWidth / states->zoom);
+				if (moveRight) states->cameraPosition.x += settings->tileWidth;
+				else if (moveLeft) states->cameraPosition.x -= settings->tileWidth;
 
 				if (moveIn) states->cameraPosition.z -= 1;
 				else if (moveOut) states->cameraPosition.z += 1;
